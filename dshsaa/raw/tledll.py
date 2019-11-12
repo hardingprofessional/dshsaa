8f8f1ec406b82b29527a7e849845a268b16ee840
David Harding <hardingprofessional@gmail.com>
2019-11-12 12:11:49 -0500

    more functions and tests for tledll

--- dshsaa/raw/tledll.py
+++ dshsaa/raw/tledll.py
@@ -73,6 +73,7 @@ def TleAddSatFrFieldsGP(satNum, secClass, satName, epochYr, epochDays, bstar, ep
 	:param float mnAnomaly: mean anomaly (degrees)
 	:param float mnMotion: mean motion (rev/day) (ephType = 0: Kozai mean motion, ephType = 2: Brouwer mean motion)
 	:param int revNum: revolution number at epoch
+	:return satKey: The satKey of the newly added TLE on success, a negative value on error.
 	"""
 	satNum = c.c_int(satNum)
 	secClass = c.c_char(secClass.encode('ascii', 'strict'))
@@ -93,9 +94,250 @@ def TleAddSatFrFieldsGP(satNum, secClass, satName, epochYr, epochDays, bstar, ep
 	return satKey
 
 ##TleAddSatFrFieldsGP2
+C_TLEDLL.TleAddSatFrFieldsGP2.restype = settings.stay_int64
+C_TLEDLL.TleAddSatFrFieldsGP2.argtypes = [c.c_int32, 
+										 c.c_char, 
+										 c.c_char_p, 
+										 c.c_int,
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_int, 
+										 c.c_int, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_int,
+										 c.c_double,
+										 c.c_double]
+def TleAddSatFrFieldsGP2(satNum, secClass, satName, epochYr, epochDays, bstar, ephType, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum, nDotO2, n2DotO6):
+	"""
+	python:function::TleAddSatFrFieldsGP2
+	This function is similar to TleAddSatFrFieldsGP but includes nDotO2 and n2DotO6. nDotO2 and n2DotO6 values are not used in the SGP4 propagator. However, some users still want to preserve the integrity of all input data. 
+	:param int satNum: satellite number
+	:param str secClass: security classification
+	:param str satName: Satellite international designator (string[8])
+	:param int epochYr: Element epoch time - year, [YY]YY
+	:param float epochDays: Element epoch time - day of year, DDD.DDDDDDDD
+	:param float bstar: B* drag term (1/er)
+	:param int ephType: Satellite ephemeris type (0: SGP, 2: SGP4)
+	:param int elsetNum: element set number
+	:param float incli: orbit inclination (degrees)
+	:param float node: right ascension of ascending node (degrees)
+	:param float eccen: eccentricity
+	:param float omega: argument of perigee (degrees)
+	:param float mnAnomaly: mean anomaly (degrees)
+	:param float mnMotion: mean motion (rev/day) (ephType = 0: Kozai mean motion, ephType = 2: Brouwer mean motion)
+	:param int revNum: revolution number at epoch
+	:param float nDotO2: Mean motion derivative (rev/day /2)
+	:param float n2DotO6: Mean motion second derivative (rev/day**2 /6)
+	:return satKey: 
+	"""
+	satNum = c.c_int(satNum)
+	secClass = c.c_char(secClass.encode('ascii', 'strict'))
+	satName = settings.str_to_c_char_p(satName, fixed_width=8)
+	epochYr = c.c_int(epochYr)
+	epochDays = c.c_double(epochDays)
+	bstar = c.c_double(bstar)
+	ephType = c.c_int32(ephType)
+	elsetNum = c.c_int32(elsetNum)
+	incli = c.c_double(incli)
+	node = c.c_double(node)
+	eccen = c.c_double(eccen)
+	omega = c.c_double(omega)
+	mnAnomaly = c.c_double(mnAnomaly)
+	mnMotion = c.c_double(mnMotion)
+	revNum = c.c_int32(revNum)
+	nDotO2 = c.c_double(nDotO2)
+	n2DotO6 = c.c_double(n2DotO6)
+	satKey = C_TLEDLL.TleAddSatFrFieldsGP2(satNum, secClass, satName, epochYr, epochDays, bstar, ephType, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum, nDotO2, n2DotO6)
+	return satKey
+	
+	
 ##TleAddSatFrFieldsGP2ML
+C_TLEDLL.TleAddSatFrFieldsGP2ML.restype = settings.stay_int64
+C_TLEDLL.TleAddSatFrFieldsGP2ML.argtypes = [c.c_int32, 
+										 c.c_char, 
+										 c.c_char_p, 
+										 c.c_int,
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_int, 
+										 c.c_int, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_double, 
+										 c.c_int,
+										 c.c_double,
+										 c.c_double]
+def TleAddSatFrFieldsGP2ML(satNum, secClass, satName, epochYr, epochDays, bstar, ephType, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum, nDotO2, n2DotO6):
+	"""
+	python:function::TleAddSatFrFieldsGP2ML
+	This function is similar to TleAddSatFrFieldsGP2 but designed to be used in Matlab. Matlab doesn't seem to correctly return the 19-digit satellite key using TleAddSatFrFieldsGP2. This method is an alternative way to return the satKey output. This function is similar to TleAddSatFrFieldsGP but includes nDotO2 and n2DotO6. nDotO2 and n2DotO6 values are not used in the SGP4 propagator. However, some users still want to preserve the integrity of all input data. 
+	:param int satNum: satellite number
+	:param str secClass: security classification
+	:param str satName: Satellite international designator (string[8])
+	:param int epochYr: Element epoch time - year, [YY]YY
+	:param float epochDays: Element epoch time - day of year, DDD.DDDDDDDD
+	:param float bstar: B* drag term (1/er)
+	:param int ephType: Satellite ephemeris type (0: SGP, 2: SGP4)
+	:param int elsetNum: element set number
+	:param float incli: orbit inclination (degrees)
+	:param float node: right ascension of ascending node (degrees)
+	:param float eccen: eccentricity
+	:param float omega: argument of perigee (degrees)
+	:param float mnAnomaly: mean anomaly (degrees)
+	:param float mnMotion: mean motion (rev/day) (ephType = 0: Kozai mean motion, ephType = 2: Brouwer mean motion)
+	:param int revNum: revolution number at epoch
+	:param float nDotO2: Mean motion derivative (rev/day /2)
+	:param float n2DotO6: Mean motion second derivative (rev/day**2 /6)
+	:return satKey: 
+	"""
+	satNum = c.c_int(satNum)
+	secClass = c.c_char(secClass.encode('ascii', 'strict'))
+	satName = settings.str_to_c_char_p(satName, fixed_width=8)
+	epochYr = c.c_int(epochYr)
+	epochDays = c.c_double(epochDays)
+	bstar = c.c_double(bstar)
+	ephType = c.c_int32(ephType)
+	elsetNum = c.c_int32(elsetNum)
+	incli = c.c_double(incli)
+	node = c.c_double(node)
+	eccen = c.c_double(eccen)
+	omega = c.c_double(omega)
+	mnAnomaly = c.c_double(mnAnomaly)
+	mnMotion = c.c_double(mnMotion)
+	revNum = c.c_int32(revNum)
+	nDotO2 = c.c_double(nDotO2)
+	n2DotO6 = c.c_double(n2DotO6)
+	satKey = C_TLEDLL.TleAddSatFrFieldsGP2ML(satNum, secClass, satName, epochYr, epochDays, bstar, ephType, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum, nDotO2, n2DotO6)
+	return satKey
+
+
 ##TleAddSatFrFieldsSP
+C_TLEDLL.TleAddSatFrFieldsSP.restype = settings.stay_int64
+C_TLEDLL.TleAddSatFrFieldsSP.argtypes = [c.c_int32,
+										 c.c_char,
+										 c.c_char_p,
+										 c.c_int32,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_int32,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_int32]
+def TleAddSatFrFieldsSP(satNum, secClass, satName, epochYr, epochDays, bTerm, ogParm, agom, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum):
+	"""
+	python:function::TleAddSatFrFieldsSP
+	Adds an SP satellite using the individually provided field values. Only applies to SP propagator.
+	Review https://www.sat.dundee.ac.uk/fle.html for more information on SP 4 line element sets.
+	:param int satNum: Satellite number
+	:param str secClass: Security classification
+	:param str satName: Satellite international designator (string[8])
+	:param int epochYr: Element epoch time - year, [YY]YY
+	:param float epochDays: Element epoch time - day of year, DDD.DDDDDDDD
+	:param float bTerm: Ballistic coefficient (m^2/kg)
+	:param float ogParm: Outgassing parameter/Thrust Acceleration (km/s^2)
+	:param float agom: Agom (radiation pressure coefficient) (m^2/kg)
+	:param int elsetNum: Element set number
+	:param float incli: Orbit inclination (degrees)
+	:param float node: Right ascension of ascending node (degrees)
+	:param float eccen: Eccentricity
+	:param float omega: Argument of perigee (degrees)
+	:param float mnAnomaly: Mean anomaly (degrees)
+	:param float mnMotion: Mean motion (rev/day)
+	:param int revNum: Revolution number at epoch
+	:return settings.stay_int64 satKey: The satKey of the newly added TLE on success, a negative value on error.
+	"""
+	satNum = c.c_int32(satNum)
+	secClass = c.c_char(secClass.encode('ascii', 'strict'))
+	satName = settings.str_to_c_char_p(satName, fixed_width=8)
+	epochYr = c.c_int(epochYr)
+	epochDays = c.c_double(epochDays)
+	bTerm = c.c_double(bTerm)
+	ogParm = c.c_double(ogParm)
+	agom = c.c_double(agom)
+	elsetNum = c.c_int32(elsetNum)
+	incli = c.c_double(incli)
+	node = c.c_double(node)
+	eccen = c.c_double(eccen)
+	omega = c.c_double(omega)
+	mnAnomaly = c.c_double(mnAnomaly)
+	mnMotion = c.c_double(mnMotion)
+	revNum = c.c_int32(revNum)
+	satKey = C_TLEDLL.TleAddSatFrFieldsSP(satNum, secClass, satName, epochYr, epochDays, bTerm, ogParm, agom, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum)
+	return satKey
+	
 ##TleAddSatFrFieldsSPML
+C_TLEDLL.TleAddSatFrFieldsSPML.restype = settings.stay_int64
+C_TLEDLL.TleAddSatFrFieldsSPML.argtypes = [c.c_int32,
+										 c.c_char,
+										 c.c_char_p,
+										 c.c_int32,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_int32,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_double,
+										 c.c_int32]
+def TleAddSatFrFieldsSPML(satNum, secClass, satName, epochYr, epochDays, bTerm, ogParm, agom, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum):
+	"""
+	python:function::TleAddSatFrFieldsSPML
+	Adds an SP satellite using the individually provided field values. This function is similar to TleAddSatFrFieldsSP but designed to be used in Matlab. Matlab doesn't correctly return the 19-digit satellite key using TleAddSatFrFieldsSP. This method is an alternative way to return the satKey output. Only applies to SP propagator.
+	Review https://www.sat.dundee.ac.uk/fle.html for more information on SP 4 line element sets.
+	:param int satNum: Satellite number
+	:param str secClass: Security classification
+	:param str satName: Satellite international designator (string[8])
+	:param int epochYr: Element epoch time - year, [YY]YY
+	:param float epochDays: Element epoch time - day of year, DDD.DDDDDDDD
+	:param float bTerm: Ballistic coefficient (m^2/kg)
+	:param float ogParm: Outgassing parameter/Thrust Acceleration (km/s^2)
+	:param float agom: Agom (radiation pressure coefficient) (m^2/kg)
+	:param int elsetNum: Element set number
+	:param float incli: Orbit inclination (degrees)
+	:param float node: Right ascension of ascending node (degrees)
+	:param float eccen: Eccentricity
+	:param float omega: Argument of perigee (degrees)
+	:param float mnAnomaly: Mean anomaly (degrees)
+	:param float mnMotion: Mean motion (rev/day)
+	:param int revNum: Revolution number at epoch
+	:return settings.stay_int64 satKey: The satKey of the newly added TLE on success, a negative value on error.
+	"""
+	satNum = c.c_int32(satNum)
+	secClass = c.c_char(secClass.encode('ascii', 'strict'))
+	satName = settings.str_to_c_char_p(satName, fixed_width=8)
+	epochYr = c.c_int(epochYr)
+	epochDays = c.c_double(epochDays)
+	bTerm = c.c_double(bTerm)
+	ogParm = c.c_double(ogParm)
+	agom = c.c_double(agom)
+	elsetNum = c.c_int32(elsetNum)
+	incli = c.c_double(incli)
+	node = c.c_double(node)
+	eccen = c.c_double(eccen)
+	omega = c.c_double(omega)
+	mnAnomaly = c.c_double(mnAnomaly)
+	mnMotion = c.c_double(mnMotion)
+	revNum = c.c_int32(revNum)
+	satKey = C_TLEDLL.TleAddSatFrFieldsSPML(satNum, secClass, satName, epochYr, epochDays, bTerm, ogParm, agom, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum)
+	return satKey
 
 ##TleAddSatFrLines
 C_TLEDLL.TleAddSatFrLines.restype = settings.stay_int64
--- test/raw/test_tledll.py
+++ test/raw/test_tledll.py
@@ -69,21 +69,132 @@ class TestTleDll(unittest.TestCase):
 		mnMotion  = 15.72125391
 		revNum    = 56353
 		satKey    = tledll.TleAddSatFrFieldsGP(satNum, secClass, satName, epochYr, epochDays, bstar, ephType, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum)
-		pdb.set_trace()
+		self.assertTrue(satKey.value > 0)
 	
 	##TleAddSatFrFieldsGP2
+	def test_TleAddSatFrFieldsGP2(self):
+		"""
+		ISS (ZARYA)
+		1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
+		2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537
+		Notes:
+		ISS (ZARYA) is an illegal name because it has > 8 characters
+		bstar comes from digits "-11606-4". To apply leading zero use formula #0.[0]{n}(digits) to yield -0.000011606
+		Eccentricity needs a leading "0."
+		Docs: 
+		https://en.wikipedia.org/wiki/Two-line_element_set#cite_note-nasahelp-12
+		https://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/SSOP_Help/tle_def.html
+		"""
+		satNum    = 25544
+		secClass  = 'U'
+		satName   = 'ISS'
+		epochYr   = 8
+		epochDays = 264.51782528
+		bstar     = -0.000011606 #0.[0]{n}(digits) to accomplish assumed decimal point
+		ephType   = 0
+		elsetNum  = 292
+		incli     = 51.6416
+		node      = 247.4627
+		eccen     = 0.0006703 #a leading decimal point is necessary
+		omega     = 130.5360
+		mnAnomaly = 325.0288
+		mnMotion  = 15.72125391
+		revNum    = 56353
+		nDotO2    = 555555 #value not used, but is stored for data integrity
+		n2DotO6   = 666666 #value not used, but is stored for data integrity
+		satKey    = tledll.TleAddSatFrFieldsGP2(satNum, secClass, satName, epochYr, epochDays, bstar, ephType, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum, nDotO2, n2DotO6)
+		self.assertTrue(satKey.value > 0)
+
+	@unittest.skip("Segmentation fault, matlab")
 	##TleAddSatFrFieldsGP2ML
-	##TleAddSatFrFieldsSP
-	##TleAddSatFrFieldsSPML
+	def test_TleAddSatFrFieldsGP2ML(self):
+		"""
+		ISS (ZARYA)
+		1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
+		2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537
+		Notes:
+		ISS (ZARYA) is an illegal name because it has > 8 characters
+		bstar comes from digits "-11606-4". To apply leading zero use formula #0.[0]{n}(digits) to yield -0.000011606
+		Eccentricity needs a leading "0."
+		Docs: 
+		https://en.wikipedia.org/wiki/Two-line_element_set#cite_note-nasahelp-12
+		https://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/SSOP_Help/tle_def.html
+		"""
+		satNum    = 25544
+		secClass  = 'U'
+		satName   = 'ISS'
+		epochYr   = 8 #2008
+		epochDays = 264.51782528
+		bstar     = -0.000011606 #0.[0]{n}(digits) to accomplish assumed decimal point
+		ephType   = 0
+		elsetNum  = 292
+		incli     = 51.6416
+		node      = 247.4627
+		eccen     = 0.0006703 #a leading decimal point is necessary
+		omega     = 130.5360
+		mnAnomaly = 325.0288
+		mnMotion  = 15.72125391
+		revNum    = 56353
+		nDotO2    = 555555 #value not used, but is stored for data integrity
+		n2DotO6   = 666666 #value not used, but is stored for data integrity
+		satKey    = tledll.TleAddSatFrFieldsGP2ML(satNum, secClass, satName, epochYr, epochDays, bstar, ephType, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum, nDotO2, n2DotO6)
+		self.assertTrue(satKey.value > 0)
 	
+	##TleAddSatFrFieldsSP
+	def test_TleAddSatFrFieldsSP(self):
+		"""
+		https://www.sat.dundee.ac.uk/fle.html
+		"""
+		satNum    = 21263
+		secClass  = 'U'
+		satName   = 'Dundee'
+		epochYr   = 94
+		epochDays = 264.51782528
+		bTerm     = 0.06822045
+		ogParm    = 0.001
+		agom      = .0000500000
+		elsetNum  = 292
+		incli     = 51.6416
+		node      = 247.4627
+		eccen     = 0.0006703
+		omega     = 130.5360
+		mnAnomaly = 325.0288
+		mnMotion  = 15.72125391
+		revNum    = 15390
+		satKey    = tledll.TleAddSatFrFieldsSP(satNum, secClass, satName, epochYr, epochDays, bTerm, ogParm, agom, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum)
+		self.assertTrue(satKey.value > 0)
+		
+	##TleAddSatFrFieldsSPML
+	@unittest.skip("Segmentation fault, matlab")
+	def test_TleAddSatFrFieldsSPML(self):
+		"""
+		https://www.sat.dundee.ac.uk/fle.html
+		"""
+		satNum    = 21263
+		secClass  = 'U'
+		satName   = 'Dundee'
+		epochYr   = 94
+		epochDays = 264.51782528
+		bTerm     = 0.06822045
+		ogParm    = 0.001
+		agom      = .0000500000
+		elsetNum  = 292
+		incli     = 51.6416
+		node      = 247.4627
+		eccen     = 0.0006703
+		omega     = 130.5360
+		mnAnomaly = 325.0288
+		mnMotion  = 15.72125391
+		revNum    = 15390
+		satKey    = tledll.TleAddSatFrFieldsSPML(satNum, secClass, satName, epochYr, epochDays, bTerm, ogParm, agom, elsetNum, incli, node, eccen, omega, mnAnomaly, mnMotion, revNum)
+		self.assertTrue(satKey.value > 0)
+		
 	##TleAddSatFrLines
 	def test_TleAddSatFrLines(self):
 		line1 = '1 23455U 94089A   97320.90946019  .00000140  00000-0  10191-3 0  2621'
 		line2 = '2 23455  99.0090 272.6745 0008546 223.1686 136.8816 14.11711747148495'
-		
 		satKey = tledll.TleAddSatFrLines(line1, line2)
-		#pdb.set_trace()
-		#self.assertTrue(satKey.value > 0)
+		self.assertTrue(satKey.value > 0)
 	
 	##TleAddSatFrLinesML
 	##TleDataToArray
